5392be56bd00be7564ade246f238fbe21ce74b1d
blackbaud-johnly <John.Lyons@blackbaud.com>
2014-11-05 16:59:29 -0500

    Add files and update readme

---
+++ AddAddressWinFormWebAPIClient.dll/AddAddressWinFormWebAPIClient.dll/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAddAddress = New System.Windows.Forms.Button
+        Me.lblLookupID = New System.Windows.Forms.Label
+        Me.lblStreetAddress = New System.Windows.Forms.Label
+        Me.lblState = New System.Windows.Forms.Label
+        Me.txtLookupID = New System.Windows.Forms.TextBox
+        Me.txtAddress = New System.Windows.Forms.TextBox
+        Me.cboState = New System.Windows.Forms.ComboBox
+        Me.cboAddressType = New System.Windows.Forms.ComboBox
+        Me.lblAddressType = New System.Windows.Forms.Label
+        Me.txtSearchResults = New System.Windows.Forms.TextBox
+        Me.btnSearch = New System.Windows.Forms.Button
+        Me.DataGridViewAddress = New System.Windows.Forms.DataGridView
+        Me.btnDeleteAddress = New System.Windows.Forms.Button
+        CType(Me.DataGridViewAddress, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnAddAddress
+        '
+        Me.btnAddAddress.Enabled = False
+        Me.btnAddAddress.Location = New System.Drawing.Point(329, 145)
+        Me.btnAddAddress.Name = "btnAddAddress"
+        Me.btnAddAddress.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddAddress.TabIndex = 0
+        Me.btnAddAddress.Text = "Add Address"
+        Me.btnAddAddress.UseVisualStyleBackColor = True
+        '
+        'lblLookupID
+        '
+        Me.lblLookupID.AutoSize = True
+        Me.lblLookupID.Location = New System.Drawing.Point(24, 8)
+        Me.lblLookupID.Name = "lblLookupID"
+        Me.lblLookupID.Size = New System.Drawing.Size(60, 13)
+        Me.lblLookupID.TabIndex = 1
+        Me.lblLookupID.Text = "Lookup ID:"
+        '
+        'lblStreetAddress
+        '
+        Me.lblStreetAddress.AutoSize = True
+        Me.lblStreetAddress.Location = New System.Drawing.Point(36, 49)
+        Me.lblStreetAddress.Name = "lblStreetAddress"
+        Me.lblStreetAddress.Size = New System.Drawing.Size(48, 13)
+        Me.lblStreetAddress.TabIndex = 2
+        Me.lblStreetAddress.Text = "Address:"
+        '
+        'lblState
+        '
+        Me.lblState.AutoSize = True
+        Me.lblState.Location = New System.Drawing.Point(49, 123)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(35, 13)
+        Me.lblState.TabIndex = 3
+        Me.lblState.Text = "State:"
+        '
+        'txtLookupID
+        '
+        Me.txtLookupID.Location = New System.Drawing.Point(90, 5)
+        Me.txtLookupID.Name = "txtLookupID"
+        Me.txtLookupID.Size = New System.Drawing.Size(127, 20)
+        Me.txtLookupID.TabIndex = 4
+        Me.txtLookupID.Text = "96"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(90, 49)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(314, 65)
+        Me.txtAddress.TabIndex = 6
+        '
+        'cboState
+        '
+        Me.cboState.FormattingEnabled = True
+        Me.cboState.Location = New System.Drawing.Point(90, 120)
+        Me.cboState.Name = "cboState"
+        Me.cboState.Size = New System.Drawing.Size(127, 21)
+        Me.cboState.TabIndex = 7
+        '
+        'cboAddressType
+        '
+        Me.cboAddressType.FormattingEnabled = True
+        Me.cboAddressType.Location = New System.Drawing.Point(90, 147)
+        Me.cboAddressType.Name = "cboAddressType"
+        Me.cboAddressType.Size = New System.Drawing.Size(127, 21)
+        Me.cboAddressType.TabIndex = 9
+        '
+        'lblAddressType
+        '
+        Me.lblAddressType.AutoSize = True
+        Me.lblAddressType.Location = New System.Drawing.Point(12, 150)
+        Me.lblAddressType.Name = "lblAddressType"
+        Me.lblAddressType.Size = New System.Drawing.Size(75, 13)
+        Me.lblAddressType.TabIndex = 8
+        Me.lblAddressType.Text = "Address Type:"
+        '
+        'txtSearchResults
+        '
+        Me.txtSearchResults.BackColor = System.Drawing.SystemColors.Control
+        Me.txtSearchResults.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtSearchResults.Location = New System.Drawing.Point(90, 30)
+        Me.txtSearchResults.Name = "txtSearchResults"
+        Me.txtSearchResults.Size = New System.Drawing.Size(284, 13)
+        Me.txtSearchResults.TabIndex = 10
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Enabled = False
+        Me.btnSearch.Location = New System.Drawing.Point(223, 3)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(56, 23)
+        Me.btnSearch.TabIndex = 11
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'DataGridViewAddress
+        '
+        Me.DataGridViewAddress.AllowUserToAddRows = False
+        Me.DataGridViewAddress.AllowUserToDeleteRows = False
+        Me.DataGridViewAddress.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewAddress.Location = New System.Drawing.Point(12, 174)
+        Me.DataGridViewAddress.Name = "DataGridViewAddress"
+        Me.DataGridViewAddress.ReadOnly = True
+        Me.DataGridViewAddress.Size = New System.Drawing.Size(392, 190)
+        Me.DataGridViewAddress.TabIndex = 12
+        '
+        'btnDeleteAddress
+        '
+        Me.btnDeleteAddress.Enabled = False
+        Me.btnDeleteAddress.Location = New System.Drawing.Point(248, 370)
+        Me.btnDeleteAddress.Name = "btnDeleteAddress"
+        Me.btnDeleteAddress.Size = New System.Drawing.Size(156, 23)
+        Me.btnDeleteAddress.TabIndex = 13
+        Me.btnDeleteAddress.Text = "Delete Selected Address"
+        Me.btnDeleteAddress.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 396)
+        Me.Controls.Add(Me.btnDeleteAddress)
+        Me.Controls.Add(Me.DataGridViewAddress)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtSearchResults)
+        Me.Controls.Add(Me.cboAddressType)
+        Me.Controls.Add(Me.lblAddressType)
+        Me.Controls.Add(Me.cboState)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtLookupID)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.lblStreetAddress)
+        Me.Controls.Add(Me.lblLookupID)
+        Me.Controls.Add(Me.btnAddAddress)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridViewAddress, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAddAddress As System.Windows.Forms.Button
+    Friend WithEvents lblLookupID As System.Windows.Forms.Label
+    Friend WithEvents lblStreetAddress As System.Windows.Forms.Label
+    Friend WithEvents lblState As System.Windows.Forms.Label
+    Friend WithEvents txtLookupID As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents cboState As System.Windows.Forms.ComboBox
+    Friend WithEvents cboAddressType As System.Windows.Forms.ComboBox
+    Friend WithEvents lblAddressType As System.Windows.Forms.Label
+    Friend WithEvents txtSearchResults As System.Windows.Forms.TextBox
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents DataGridViewAddress As System.Windows.Forms.DataGridView
+    Friend WithEvents btnDeleteAddress As System.Windows.Forms.Button
+
+End Class
---
+++ AddAddressWinFormWebAPIClient.dll/AddAddressWinFormWebAPIClient.dll/Form1.vb
@@ -0,0 +1,303 @@
+﻿Imports Blackbaud.AppFx.Constituent.Catalog.WebApiClient
+Imports Blackbaud.AppFx.WebAPI
+
+Public Class Form1
+    Dim ConstituentID As String
+    Dim ServiceProvider As AppFxWebServiceProvider
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Initialize()
+    End Sub
+
+    Private Sub btnSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+        ResetForm()
+        RetrieveConstituent()
+        RetrieveAddress()
+        EvalAddressDisplay()
+    End Sub
+
+    Private Sub btnAddAddress_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAddAddress.Click
+        'Create a new form data object
+        Dim AddressAddForm2Data As AddForms.Address.AddressAddForm2Data
+
+        'Load defaults via LoadData
+        AddressAddForm2Data = AddForms.Address.AddressAddForm2.LoadData(ServiceProvider, ConstituentID)
+
+        'Update data from control on the UI
+        AddressAddForm2Data.ADDRESSBLOCK = Me.txtAddress.Text.ToString
+        If Me.cboState.SelectedIndex = -1 Then
+        Else
+            AddressAddForm2Data.STATEID = New System.Guid(CType(Me.cboState.SelectedItem, SimpleState).StateID)
+        End If
+
+        If Me.cboAddressType.SelectedIndex = -1 Then
+        Else
+            AddressAddForm2Data.ADDRESSTYPECODEID = CType(Me.cboAddressType.SelectedItem, SimpleAddressType).AddressType
+        End If
+
+
+        AddressAddForm2Data.Save(ServiceProvider)
+        RetrieveAddress()
+        EvalAddressDisplay()
+    End Sub
+
+    Private Sub txtAddress_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtAddress.TextChanged
+        Me.btnAddAddress.Enabled = EvalAddressCriteriaDisplay()
+    End Sub
+
+    Private Sub btnDeleteAddress_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDeleteAddress.Click
+        Try
+            If DataGridViewAddress.RowCount = 0 Then
+
+            ElseIf DataGridViewAddress.RowCount > 0 Then
+
+                If DataGridViewAddress.SelectedRows.Count > 0 Then
+                    For Each Row As Windows.Forms.DataGridViewRow In DataGridViewAddress.SelectedRows
+                        RecordOperations.Address.AddressDelete.ExecuteOperation(ServiceProvider, Row.Cells(0).Value.ToString)
+                    Next
+                End If
+
+            End If
+
+        Catch ex As Blackbaud.AppFx.WebAPI.AppFxWebServiceException
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Primary Address")
+        Catch ex As Exception
+           
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error Deleting Address")
+        Finally
+            RetrieveAddress()
+            EvalAddressDisplay()
+        End Try
+
+    End Sub
+
+#Region "Helper Functions"
+    Private Sub Initialize()
+
+        ServiceProvider = New AppFxWebServiceProvider("http://localhost/BBInfinityPROD2.7.1633.0/appfxwebservice.asmx", _
+                                                     "BBInfinityPROD2.7.1633.0", _
+                                                     "AddAddressWinFormWebAPIClient")
+        ServiceProvider.Credentials = System.Net.CredentialCache.DefaultCredentials
+
+        Me.btnSearch.Enabled = EvalSearchCriteriaDisplay()
+
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        'Initialize States
+        'Using the GUID that identifies the State Abbreviation List (Simple Data List_
+        'Retrieve a listing of states and populate the combo box with a key, value pair.
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        Dim States() As SimpleDataListsData
+        States = SimpleDataLists.LoadSimpleDataList(ServiceProvider, New System.Guid("7fa91401-596c-4f7c-936d-6e41683121d7"))
+
+        For Each State As SimpleDataListsData In States
+            Me.cboState.Items.Add(New SimpleState(State.Value, State.Label))
+        Next
+
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        'Initialize Address Types
+        'Grab a list of address types from the AddressType code table
+        'Populate the address types combo box with a key, value pair
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        Dim AddressTypes() As Blackbaud.AppFx.WebAPI.CodeTableEntryItem
+        AddressTypes = CodeTables.AddressType.GetList(ServiceProvider, CodeTableEntryIncludeOption.IncludeActiveOnly)
+
+        For Each AddressType As CodeTableEntryItem In AddressTypes
+            Me.cboAddressType.Items.Add(New SimpleAddressType(AddressType.EntryID, AddressType.Description))
+        Next
+    End Sub
+
+    Private Sub ResetForm()
+        Me.txtAddress.Text = ""
+        Me.cboAddressType.SelectedIndex = -1
+        Me.cboState.SelectedIndex = -1
+        ConstituentID = Nothing
+        Me.DataGridViewAddress.DataSource = Nothing
+
+    End Sub
+
+    Private Function EvalSearchCriteriaDisplay() As Boolean
+        Return CBool(Len(Me.txtLookupID.Text.ToString) > 0)
+    End Function
+
+    Private Function EvalAddressCriteriaDisplay() As Boolean
+        Dim AddressLength As Integer = Me.txtAddress.TextLength
+        Dim SearchResults As Integer = Me.txtSearchResults.TextLength
+
+        If AddressLength > 0 And SearchResults > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub EvalAddressDisplay()
+        If DataGridViewAddress.RowCount = 0 Then
+            Me.btnDeleteAddress.Enabled = False
+        ElseIf DataGridViewAddress.RowCount > 0 Then
+
+            DataGridViewAddress.Rows(0).Selected = True
+            Me.btnDeleteAddress.Enabled = True
+        End If
+    End Sub
+
+    Private Sub RetrieveConstituent()
+        Dim SearchFilter As New SearchLists.Constituent.IndividualSearchFilterData
+        Dim SearchResultsIDs As String()
+        SearchFilter.LOOKUPID = Me.txtLookupID.Text.ToString
+        SearchFilter.INCLUDEINDIVIDUALS = True
+        SearchFilter.INCLUDEGROUPS = False
+        SearchFilter.INCLUDEINACTIVE = False
+        SearchFilter.INCLUDENONCONSTITUENTRECORDS = False
+        SearchFilter.INCLUDEDECEASED = False
+        SearchFilter.INCLUDEORGANIZATIONS = False
+
+        SearchResultsIDs = SearchLists.Constituent.IndividualSearch.GetIDs(ServiceProvider, _
+                                                                           SearchFilter)
+        If SearchResultsIDs.Count = 0 Then
+            Me.txtSearchResults.Text = "Constituent Not Found"
+            Return
+        ElseIf SearchResultsIDs.Count > 1 Then
+            Me.txtSearchResults.Text = "Too Many Constituents Found"
+            Return
+        ElseIf SearchResultsIDs.Count = 1 Then
+            ConstituentID = SearchResultsIDs(0).ToString
+        End If
+
+        Dim ConstituentNameData As ViewForms.Constituent.ConstituentFirstNameLastNameViewFormData
+        ConstituentNameData = ViewForms.Constituent.ConstituentFirstNameLastNameViewForm.LoadData(ServiceProvider, ConstituentID)
+
+        Me.txtSearchResults.Text = ConstituentNameData.FIRSTNAME & " " & ConstituentNameData.KEYNAME
+    End Sub
+
+    Private Sub RetrieveAddress()
+        If ConstituentID = Nothing Then
+            DataGridViewAddress.DataSource = Nothing
+            DataGridViewAddress.Refresh()
+        Else
+            Dim AddressItems() As DataLists.Constituent.ContactInformationListRow
+            Dim Filter As New DataLists.Constituent.ContactInformationListFilterData
+
+            Filter.INCLUDEADDRESSES = True
+            Filter.INCLUDEFORMER = True
+            Filter.INCLUDEEMAIL = False
+            Filter.INCLUDEPHONES = False
+
+            AddressItems = DataLists.Constituent.ContactInformationList.GetRows(ServiceProvider, ConstituentID, Filter)
+
+            ' Fill in the data grid with a List
+            Dim list = New List(Of Address)
+            For Each AddressItem As DataLists.Constituent.ContactInformationListRow In AddressItems
+                list.Add(New Address(AddressItem.ID.ToString, AddressItem.CONTACTINFO, AddressItem.TYPE))
+            Next
+
+            DataGridViewAddress.DataSource = list
+            DataGridViewAddress.Columns(0).Visible = False
+            DataGridViewAddress.Columns(1).Width = 210
+            DataGridViewAddress.Columns(2).Width = 115
+            DataGridViewAddress.Refresh()
+        End If
+    End Sub
+
+    
+
+#End Region
+
+    
+End Class
+
+#Region "Helper Classes for States and Addresses"
+
+Public Class SimpleState
+    Private _key As Object ' Guid for the state
+    Private _value As String ' State Abbrev
+    Public Sub New(ByVal StateGUID As Object, ByVal StateAbbrev As String)
+        _key = StateGUID
+        _value = StateAbbrev
+    End Sub
+
+    Public ReadOnly Property StateID() As String
+        Get
+            Return _key.ToString
+        End Get
+    End Property
+
+    Public ReadOnly Property StateAbbrev() As String
+        Get
+            Return _value
+        End Get
+    End Property
+
+
+    Public Overrides Function ToString() As String
+        Return _value
+    End Function
+
+End Class
+
+Public Class SimpleAddressType
+    Private _key As Object ' Guid for the address type
+    Private _value As String ' address type description
+    Public Sub New(ByVal Key As Object, ByVal Value As String)
+        _key = Key
+        _value = Value
+    End Sub
+
+    Public ReadOnly Property AddressType() As System.Guid
+        Get
+            Return New System.Guid(_key.ToString)
+        End Get
+    End Property
+
+    Public ReadOnly Property AddressTypeDesc() As String
+        Get
+            Return _value
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return _value
+    End Function
+
+End Class
+
+''' <summary>
+''' This class contains two properties.
+''' </summary>
+Public Class Address
+    Dim _StreetAddress As String
+    Dim _AddressTypeDesc As String
+    Dim _AddressID As String
+
+    Public Sub New(ByVal AddressID As String, ByVal StreetAddress As String, ByVal AddressTypeDesc As String)
+        _AddressID = AddressID
+        _StreetAddress = StreetAddress
+        _AddressTypeDesc = AddressTypeDesc
+    End Sub
+
+    Public ReadOnly Property AddressID() As String
+        Get
+            Return _AddressID
+        End Get
+    End Property
+
+    Public ReadOnly Property StreetAddress() As String
+        Get
+            Return _StreetAddress
+        End Get
+    End Property
+
+    Public ReadOnly Property AddressTypeDesc() As String
+        Get
+            Return _AddressTypeDesc
+        End Get
+    End Property
+End Class
+
+#End Region
+
+
+
+
+
+
+
